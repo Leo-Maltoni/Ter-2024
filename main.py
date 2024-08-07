8e648a6ab161b88cf27c37bdb1692fd6b702861c
leoma <leo.maltoni5@hotmail.com>
2024-08-07 22:30:49 +0200

    add main

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
-################################
-# Main file to prepare dataset #
-################################
+import json,h5py
+from scripts.data import get_mask,get_data,get_altitude_data,get_meta
 
+def serialize_meta(meta): 
+    #serialize in json 
+    meta_serialized = json.dumps({
+    'driver': meta['driver'],
+    'dtype': meta['dtype'],
+    'nodata': meta['nodata'],
+    'width': meta['width'],
+    'height': meta['height'],
+    'count': meta['count'],
+    'crs': str(meta['crs']),  # Convert the CRS object to a string
+    'transform': str(meta['transform'])  # Convert the Affine object to a string
+    })
 
-# download data
+    return meta_serialized
 
-# get paths
+def create_hdf5_from_data(sentinel_data, hdf5_filename='hypercube.h5'):
+    with h5py.File(hdf5_filename, 'w') as hdf_file:
+
+        #Retrieve meta
+        meta = get_meta()
+
+        # Save altitude data
+        altitude_data =  get_altitude_data(meta) 
+        hdf_file.create_dataset('altitude', data=altitude_data, compression='gzip', compression_opts=9)
+
+        # Save combined mask data
+        mask = get_mask(meta)
+        hdf_file.create_dataset('mask', data=mask.astype(bool), compression='gzip', compression_opts=9)
+
+        # Save data for each month
+        for year in sentinel_data.keys():
+            for month in sentinel_data[year].keys():
+                dataset_name = f"{month}"               
+                hdf_file.create_dataset(dataset_name, data=sentinel_data[year][month], compression='gzip', compression_opts=9)
+
+            print(1,"/12")
+        # Save metadata
+        meta_serialized = serialize_meta(meta)
+        hdf_file.attrs['meta'] = meta_serialized
+
+# Retrieve data using get_data function
+sentinel_data = get_data()
+
+# Create HDF5 file using the retrieved data
+create_hdf5_from_data(sentinel_data)
--- scripts/data.py
+++ scripts/data.py
@@ -3,9 +3,14 @@ import rasterio
 from rasterio.mask import geometry_mask
 import numpy as np
 import scipy.io
-
+from rasterio.warp import calculate_default_transform, reproject, Resampling
 from scripts.paths import get_elevation_data_path, get_geojson_path, get_sentinel_data_path
 
+def calculate_ndvi(red, nir):
+    # NDVI calculation: (NIR - Red) / (NIR + Red)
+    ndvi = (nir - red) / (nir + red)
+    ndvi[np.isinf(ndvi)] = np.nan
+    return ndvi
 
 def get_mask(meta):
     gdf = gpd.read_file(get_geojson_path())
@@ -25,8 +30,9 @@ def get_mask(meta):
 
 def get_data(load_saved=False):
     if load_saved:
+        print("lik")
         return scipy.io.loadmat('./data/data.mat')['data'].keys()
-    
+    print("hhh")
     sentinel_data_path = get_sentinel_data_path()
     years = map(str, list(sentinel_data_path.keys()))
     meta = None 
@@ -36,19 +42,18 @@ def get_data(load_saved=False):
         sentinel_data[year] = {}
         months = map(str, sorted(list(sentinel_data_path[int(year)].keys())))
         for month in months:
-            sentinel_data[year][month] = {}
             bands = sentinel_data_path[int(year)][int(month)]
             blue = rasterio.open(bands["B02"]).read(1).astype(np.float32)
             green = rasterio.open(bands["B03"]).read(1).astype(np.float32)
             red = rasterio.open(bands["B04"]).read(1).astype(np.float32)
             if meta == None:
                 nir = rasterio.open(bands["B08"])
-                meta = nir.meta # They all have the same meta
+                meta = nir.meta  # They all have the same meta
                 nir = nir.read(1).astype(np.float32)
             else:
                 nir = rasterio.open(bands["B08"]).read(1).astype(np.float32)
         
-            if mask == None:
+            if mask is None:
                 mask = get_mask(meta) 
 
             blue[~mask] = np.nan
@@ -56,13 +61,39 @@ def get_data(load_saved=False):
             red[~mask] = np.nan
             nir[~mask] = np.nan
             
-            sentinel_data[year][month]["B02"] = blue
-            sentinel_data[year][month]["B03"] = green
-            sentinel_data[year][month]["B04"] = red
-            sentinel_data[year][month]["B08"] = nir 
-
-            break
+            # Calculate NDVI
+            ndvi = (nir - red) / (nir + red)
+            ndvi[~mask] = np.nan
+            
+            # Stack bands into a 3D array
+            month_data = np.stack([blue, green, red, nir, ndvi], axis=-1)
+            sentinel_data[year][month] = month_data  # Store as 3D array
     
     scipy.io.savemat('./data/data.mat', {"data":sentinel_data})
 
     return sentinel_data
+
+def get_altitude_data(meta):
+    # Resample altitude data to match the other bands
+    elevation_path = get_elevation_data_path()
+    with rasterio.open(elevation_path) as src:
+        altitude_meta = src.meta
+        transform, width, height = calculate_default_transform(
+            src.crs, meta['crs'], meta['width'], meta['height'], *src.bounds)
+        altitude_resampled = np.zeros((meta['height'], meta['width']), np.float32)
+
+        reproject(
+            source=rasterio.band(src, 1),
+            destination=altitude_resampled,
+            src_transform=src.transform,
+            src_crs=src.crs,
+            dst_transform=meta['transform'],
+            dst_crs=meta['crs'],
+            resampling=Resampling.nearest)
+        
+        return altitude_resampled
+
+def get_meta(): 
+    meta = rasterio.open(get_sentinel_data_path()[2019][1]['B02']).meta
+
+    return meta
